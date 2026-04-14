2eab478a3a62ceefa2aba52ff152022114851ead
Martin Falcon <martin.falcon@hotmail.com>
2026-04-14 16:08:41 -0600

    enviroments

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://tu-proyecto.supabase.co',
-  supabaseAnonKey: 'tu-anon-key'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://tu-proyecto.supabase.co',
-  supabaseAnonKey: 'tu-anon-key'
-};
